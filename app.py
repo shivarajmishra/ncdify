3b9a0aab952258a6864240cfce81b8bc9c276d52
Shiva Raj Mishra <shivarajmishra@gmail.com>
2022-02-18 23:36:27 +1100

    new changes

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import plotly.express as px
 
 import plotly.graph_objects as go
 import dash
-from dash import dcc
-from dash import html
+import dash_core_components as dcc
+import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 
